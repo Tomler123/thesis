d082bb150760d8a78f4d88e88ab8c22955aa6eff
Toma Sulaberidze <nikolonikoloa@outlook.com>
2024-05-03 07:13:51 +0200

    Fix Heroku port assignment

--- app.py
+++ app.py
@@ -1675,4 +1675,6 @@ def get_response():
     return jsonify({'message': str(response)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    # app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
